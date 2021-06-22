4dd3f5d662894fe88b2823aff092980183eaf238
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-22 20:27:13 +0530

    MODIFIED:SnakeAndLadder class and determining player's current position and repeating player position till it reaches exact position 100

--- src/com/bridgelab/SnakeAndLadder.java
+++ src/com/bridgelab/SnakeAndLadder.java
@@ -3,7 +3,7 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         int initialPosition=0;
         System.out.println("initial position is:" + initialPosition);
-        while(initialPosition<100) {
+        while(initialPosition<=100) {
             double diceNumber = Math.floor(Math.random() * 10) % 6 + 1;
             System.out.println("dice number is:" + diceNumber);
             double currentPosition = Math.floor(Math.random() * 10) % 3;
