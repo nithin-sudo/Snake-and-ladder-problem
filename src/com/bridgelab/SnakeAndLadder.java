2e5e23ee1621edf9af3a19e495e3f5105221ab1f
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-06-22 20:10:56 +0530

    MODIFIED:SnakeAndLadder class and generated dice number

--- src/com/bridgelab/SnakeAndLadder.java
+++ src/com/bridgelab/SnakeAndLadder.java
@@ -3,5 +3,7 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         int initialPosition=0;
         System.out.println("initial position is:" + initialPosition);
+        double diceNumber= Math.floor(Math.random()*10)%6+1;
+        System.out.println("dice number is:"+ diceNumber);
     }
 }
